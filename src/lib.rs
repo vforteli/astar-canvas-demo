9020923569c4d4602506a86a4bd34e5ece3b612c
Verner Fortelius <verner@fortelius.net>
2023-01-19 14:11:45 +0200

    we got paint!

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,48 @@ extern {
 pub fn greet() {
     alert("Hello, astar-rust-wasm!");
 }
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Board {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Board {
+    pub fn new() -> Self {
+        let width = 500;
+        let height = 500;
+
+        let cells = (0..width * height)
+            .map(|i| if i % 7 == 0 { Cell::Alive } else { Cell::Dead })
+            .collect();
+
+        Board {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+}
--- www/index.js
+++ www/index.js
@@ -1,15 +1,39 @@
-import * as wasm from "hello-wasm-pack";
+import { Board, Cell } from "astar-wasm";
+import { memory } from "astar-wasm/astar_rust_wasm_bg.wasm";
 
 // wasm.greet();
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+const board = Board.new()
+const width = board.width()
+const height = board.height()
 
 const canvas = document.getElementById("board-canvas")
-canvas.height = 500
-canvas.width = 500
+canvas.height = height
+canvas.width = width
+
+const getIndex = (row, column) => {
+    return row * width + column;
+}
 
 const context = canvas.getContext('2d');
-
 if (context) {
-    context.moveTo(0, 0)
-    context.lineTo(250, 250)
-    context.stroke()
+
+
+    const cellsPtr = board.cells()
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    context.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const index = getIndex(row, col);
+
+            context.fillStyle = cells[index] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+            context.fillRect(col, row, 1, 1)
+        }
+    }
+
+    context.stroke();
 }
