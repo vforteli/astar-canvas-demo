1b08eee05e834d275f819af807fb6f72285b0b05
Verner Fortelius <verner@fortelius.net>
2023-01-19 12:06:01 +0200

    canvas

--- www/index.js
+++ www/index.js
@@ -1,3 +1,15 @@
 import * as wasm from "hello-wasm-pack";
 
-wasm.greet();
+// wasm.greet();
+
+const canvas = document.getElementById("board-canvas")
+canvas.height = 500
+canvas.width = 500
+
+const context = canvas.getContext('2d');
+
+if (context) {
+    context.moveTo(0, 0)
+    context.lineTo(250, 250)
+    context.stroke()
+}
