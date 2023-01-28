1b6c79d62f845ecc27066075c183d5cef6006fb5
Verner Fortelius <verner@fortelius.net>
2023-01-28 11:56:51 +0200

    cleanup

--- www/src/index.ts
+++ www/src/index.ts
@@ -22,96 +22,99 @@ const pathInfoSpan = document.getElementById("path-info") as HTMLElement
 const context = canvas.getContext('2d');
 
 type Pointy = {
-    x: number,
-    y: number,
+    x: Readonly<number>,
+    y: Readonly<number>,
 }
+
+const coordinateToPointy = (x: number, y: number): Pointy => ({
+    x: Math.floor(x / (CELL_SIZE / devicePixelRatio)),
+    y: Math.floor(y / (CELL_SIZE / devicePixelRatio))
+})
+
 
 let from: Pointy | undefined = undefined;
 let to: Pointy | undefined = undefined;
 
 
 const renderImage = (context: CanvasRenderingContext2D) => {
-    if (context) {
-        const buffer = new Uint8Array(memory.buffer, board.render(), width * height * 4)
-        const imageDataRaw = new Uint8ClampedArray(buffer)
+    const imageDataRaw = new Uint8Array(memory.buffer, board.render(), width * height * 4)
+    // const imageDataRaw = new Uint8ClampedArray(buffer)
 
-        for (let row = 0; row < height; row++) {
-            for (let col = 0; col < width; col++) {
-                const pixelIndex = row * (width * 4) + (col * 4)
-
-                const r = imageDataRaw[pixelIndex]
-                const g = imageDataRaw[pixelIndex + 1]
-                const b = imageDataRaw[pixelIndex + 2]
-                const a = imageDataRaw[pixelIndex + 3]
-
-                context.fillStyle = `rgba(${r},${g},${b},1.0)`
-
-                context.fillRect(
-                    col * CELL_SIZE,
-                    row * CELL_SIZE,
-                    CELL_SIZE,
-                    CELL_SIZE
-                );
-            }
-        }
-
-        // vertical grid
+    for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
-            context.beginPath();
-            context.fillStyle = `rgba(255,255,255,0.2)`
-            context.lineWidth = 0.1;
-            context.moveTo(col * CELL_SIZE, 0);
-            context.lineTo(col * CELL_SIZE, height * CELL_SIZE);
-            context.stroke();
-        }
+            const pixelIndex = row * (width * 4) + (col * 4)
 
-        // horizontal grid
-        for (let row = 0; row < height; row++) {
-            context.beginPath();
-            context.fillStyle = `rgba(255,255,255,0.2)`
-            context.lineWidth = 0.1;
-            context.moveTo(0, row * CELL_SIZE);
-            context.lineTo(width * CELL_SIZE, row * CELL_SIZE);
-            context.stroke();
+            const r = imageDataRaw[pixelIndex]
+            const g = imageDataRaw[pixelIndex + 1]
+            const b = imageDataRaw[pixelIndex + 2]
+            const a = imageDataRaw[pixelIndex + 3]
+
+            context.fillStyle = `rgba(${r},${g},${b},1.0)`
+
+            context.fillRect(
+                col * CELL_SIZE,
+                row * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE
+            );
         }
     }
+
+    drawGrid(context)
+}
+
+const drawGrid = (context: CanvasRenderingContext2D) => {
+    context.beginPath();
+    context.fillStyle = `rgba(255,255,255,0.2)`
+    context.lineWidth = 0.1;
+
+    // vertical grid
+    for (let col = 0; col < width; col++) {
+        context.moveTo(col * CELL_SIZE, 0);
+        context.lineTo(col * CELL_SIZE, height * CELL_SIZE);
+    }
+
+    // horizontal grid
+    for (let row = 0; row < height; row++) {
+        context.moveTo(0, row * CELL_SIZE);
+        context.lineTo(width * CELL_SIZE, row * CELL_SIZE);
+    }
+
+    context.stroke();
 }
 
 
 if (context) {
-    canvas.addEventListener('click', e => {
-        const x = Math.floor(e.offsetX / (CELL_SIZE / devicePixelRatio))
-        const y = Math.floor(e.offsetY / (CELL_SIZE / devicePixelRatio))
+    canvas.onclick = e => {
+        const point = coordinateToPointy(e.offsetX, e.offsetY)
 
         if (!from) {
-            from = { x, y }
-            board.click_cell(x, y)
+            from = point
+            board.click_cell(point.x, point.y)
+
         }
         else {
-            to = { x, y }
+            to = point
             const distance = board.calculate_path(Point.new(from.x, from.y), Point.new(to.x, to.y), 1)
             pathInfoSpan.innerText = `distance: ${distance?.toFixed(2)}`
         }
 
         renderImage(context)
-    }, false);
+    }
 
-    canvas.addEventListener('contextmenu', e => {
-        const x = Math.floor(e.offsetX / (CELL_SIZE / devicePixelRatio))
-        const y = Math.floor(e.offsetY / (CELL_SIZE / devicePixelRatio))
-        from = { x, y }
-        board.click_cell(x, y)
+    canvas.oncontextmenu = e => {
+        e.preventDefault()
+        pathInfoSpan.innerText = `distance: `
+        from = coordinateToPointy(e.offsetX, e.offsetY)
+        board.click_cell(from.x, from.y)
         renderImage(context)
-        e.preventDefault();
-    }, false);
+    }
 
-    canvas.addEventListener('mousemove', e => {
-        const x = Math.floor(e.offsetX / (CELL_SIZE / devicePixelRatio))
-        const y = Math.floor(e.offsetY / (CELL_SIZE / devicePixelRatio))
-
-        const cellInfo = board.get_cell_info(x, y)
-        pointInfoSpan.innerText = `x: ${x}, y: ${y}, weight: ${cellInfo?.toFixed(2)}`
-    }, false);
+    canvas.onpointermove = e => {
+        const point = coordinateToPointy(e.offsetX, e.offsetY)
+        const cellInfo = board.get_cell_info(point.x, point.y)
+        pointInfoSpan.innerText = `x: ${point.x}, y: ${point.y}, weight: ${cellInfo?.toFixed(2)}`
+    }
 
     renderImage(context)
 }
